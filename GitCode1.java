e113ccb6961f503c46a5fd17378cd8fbc2a153ef
AshuThakur118 <67106066+AshuThakur118@users.noreply.github.com>
2020-07-01 00:26:56 +0530

    Add files via upload

--- GitCode1.java
+++ GitCode1.java
@@ -2,6 +2,6 @@ import java.util.*;
 class Hello {
 
       public static void main(String args []) {
-      System.out.println("Hello My India!!!! This is M First jenkins Project");
+      System.out.println("Hello My India!!!! This is My First jenkins Project");
    }
  }
